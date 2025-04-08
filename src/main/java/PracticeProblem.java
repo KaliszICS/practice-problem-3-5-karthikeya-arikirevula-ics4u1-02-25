5f7ba814fe31e81bb74ab6c363189753a3d74355
Karthikeya Arikirevula <karthikeya.arikirevula@student.tdsb.on.ca>
2025-04-08 20:18:09 +0000

    complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,36 @@
-public class PracticeProblem {
+public class PracticeProblem{
+	static String[][] maze;
+	static int paths;
+	static int search(int row, int col){
+			if(maze[row][col].equals("F")){
+					paths++;
+					return 0;
+			}
 
-	public static void main(String args[]) {
-
+			int min = maze.length * maze[row].length + 1;
+			if(row-1 > -1){
+					int c = search(row-1, col);
+					min = c < min ? c : min;
+			}
+			if(col+1 < maze[row].length){
+					int c = search(row, col+1);
+					min = c < min ? c : min;
+			}
+			return min+1;
 	}
 
-	
+	static int searchMazeMoves(String[][] arr){
+			maze = arr;
+			paths = 0;
+			return search(arr.length-1, 0);
+	}
 
+	static int noOfPaths(String[][] arr){
+			maze = arr;
+			paths = 0;
+			search(arr.length-1, 0);
+			return paths;
+	}
+
+	public static void main(String args[]){}
 }
